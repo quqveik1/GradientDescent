5420c00f59720aef6d4aa8aae6aeae61c612c328
Alex Bogomol <quqveik.1@gmail.com>
2023-04-02 17:26:40 +0300

    Real init commit

---
+++ CoorEngine.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <AbstractApp.h>
+
+
+struct CoorEngine : AbstractAppData
+{
+    CoorEngine() : AbstractAppData(NULL, "..\\TESTWIN32_GRAPHICAPP\\x64\\Debug") {};
+
+    virtual void onCreate(HWND window, UINT message, WPARAM wParam, LPARAM lParam) override;
+    virtual void setWindowParameters(HINSTANCE hInstance)  override;
+};
